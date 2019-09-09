b7f01ddad309609cdf81e477fb99f913c7d0e4c1
Aliaksei Kaziukovich <a.kaziukovich@softarex.com>
2019-09-09 12:19:47 +0300

    Lehmer random sequence generator class implemented

---
+++ generators/LehmerSequence.py
@@ -0,0 +1,22 @@
+import math
+import SequenceInterface
+
+class LehmerSequence(SequenceInterface.ISequence):
+    def __init__(self, a, m, r0):
+        self.__a = a
+        self.__m = m
+        self.__r0 = r0
+        self.__r = r0
+        
+    def provide_sequence(self, sequence_length):
+        random_sequence = []
+        for i in range(sequence_length):
+            random_sequence.append(self.next_number())
+        return random_sequence
+        
+    def next_number(self):
+        self.__r = self.__a * self.__r % self.__m
+        return self.__r / self.__m
+    
+    def reset(self):
+        self.__r = self.__r0
