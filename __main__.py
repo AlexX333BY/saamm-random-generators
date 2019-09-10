ecd5a4b56391cbc7f0d4bb729d6d20c94ad29377
Aliaksei <alexx333by@protonmail.com>
2019-09-10 19:09:30 +0300

    Simplified char drawing

--- __main__.py
+++ __main__.py
@@ -2,41 +2,13 @@ import argparse
 from generators.LehmerSequence import LehmerSequence
 from SequenceAnalyzer import SequenceAnalyzer
 import matplotlib.pyplot as plt
-import matplotlib
-from array import array
 
 
 def draw_chart(sequence, intervals_count=20):
-    reverse_sorted_sequence = sorted(sequence)
-    reverse_sorted_sequence.reverse()
-    sequence_length = len(sequence)
-
-    x_min = reverse_sorted_sequence[sequence_length - 1]
-    x_max = reverse_sorted_sequence[0]
-    delta = (x_max - x_min) / intervals_count
-
-    intervals_sizes = array('i')
-    for i in range(intervals_count):
-        cur_range_size = 0
-        cur_range_max = x_min + (i + 1) * delta
-        while len(reverse_sorted_sequence) > 0 \
-                and reverse_sorted_sequence[len(reverse_sorted_sequence) - 1] <= cur_range_max:
-            reverse_sorted_sequence.pop()
-            cur_range_size += 1
-        intervals_sizes.append(cur_range_size)
-
-    intervals_sizes[intervals_count - 1] += len(reverse_sorted_sequence)
-
-    labels = []
-    for i in range(intervals_count):
-        labels.append(str(x_min + i * delta)[:5])
-
-    matplotlib.rcParams['toolbar'] = 'None'
-    chart = plt.gca()
-    chart.bar(range(intervals_count), intervals_sizes)
-    chart.set_xticklabels(labels)
-    plt.grid(True)
+    plt.rcParams["patch.force_edgecolor"] = True
+    plt.hist(sequence, bins=intervals_count)
     plt.show()
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -65,6 +37,7 @@ def main():
     print("Значение косвенного признака: ", analyzer.get_indirect_sign())
 
     draw_chart(sequence)
+
 
 if __name__ == "__main__":
     main()
