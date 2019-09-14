2b6a36aad9b405677cd6ee2dbdf909beacd7a772
Aliaksei <alexx333by@protonmail.com>
2019-09-14 16:21:39 +0300

    Replaced IRandomGenerator interface with base class

--- RandomGenerators.py
+++ RandomGenerators.py
@@ -1,23 +1,22 @@
 from array import array
 
 
-class IRandomGenerator:
+class RandomGeneratorBase:
     def get_next(self): raise NotImplementedError
-    def get_sequence(self, sequence_length): raise NotImplementedError
-
-
-class LehmersUniformDistributionGenerator(IRandomGenerator):
-    def __init__(self, a, m, r0):
-        self.__a = a
-        self.__m = m
-        self.__r0 = r0
-        self.__r = r0
 
     def get_sequence(self, sequence_length):
         random_sequence = array('f')
         for i in range(sequence_length):
             random_sequence.append(self.get_next())
         return random_sequence
+
+
+class LehmersUniformDistributionGenerator(RandomGeneratorBase):
+    def __init__(self, a, m, r0):
+        self.__a = a
+        self.__m = m
+        self.__r0 = r0
+        self.__r = r0
 
     def get_next(self):
         self.__r = self.__a * self.__r % self.__m
